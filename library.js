8277aa32a18323dd581f6a7c47bdd33f07088801
Bleaze Perris <bperriscode@gmail.com>
2023-08-28 19:13:55 -0400

    added html table and form to add new books

--- library.js
+++ library.js
@@ -1,20 +1,23 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.isRead = function() {
-        if (read === true) {
-            return "read."
-        } else {
-            return "not read yet."
-        }
+}
+
+Book.prototype.isRead = function() {
+    if (read === true) {
+        return "read."
+    } else {
+        return "not read yet."
     }
-    this.info = function() {
-        return title + " by " + author + ", " + pages + " pages, " + this.isRead();
-    }
+}
+
+function addBookToLibrary() {
+
 }
 
 const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true);
 
-console.log(book1.info());
