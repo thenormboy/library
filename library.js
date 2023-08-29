9e636237422883ed5594063846cb65c4678e8f07
Bleaze Perris <bperriscode@gmail.com>
2023-08-29 19:49:42 -0400

    changed the createlist function to the displaycells function which displays info on one book only

--- library.js
+++ library.js
@@ -1,7 +1,6 @@
 const myLibrary = [];
 const container = document.querySelector('.container');
 
-
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -13,42 +12,39 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function createList() {
-    myLibrary.forEach(display_cells) 
+function display_cells(book) {
 
-    function display_cells(value) {
+    const content = document.createElement('tr');
 
-        const content = document.createElement('tr');
+    const display_title = document.createElement('td');
+    const display_author = document.createElement('td');
+    const display_pages = document.createElement('td');
+    const display_read = document.createElement('td');
 
-        const display_title = document.createElement('td');
-        const display_author = document.createElement('td');
-        const display_pages = document.createElement('td');
-        const display_read = document.createElement('td');
+    display_title.innerText = book.title
+    content.appendChild(display_title)
 
-        display_title.innerText = value.title
-        content.appendChild(display_title)
+    display_author.innerText = book.author
+    content.appendChild(display_author)
 
-        display_author.innerText = value.author
-        content.appendChild(display_author)
+    display_pages.innerText = book.pages
+    content.appendChild(display_pages)
 
-        display_pages.innerText = value.pages
-        content.appendChild(display_pages)
+    display_read.innerText = book.read
+    content.appendChild(display_read)
 
-        display_read.innerText = value.read
-        content.appendChild(display_read)
-
-        container.appendChild(content);
-    }
+    container.appendChild(content);
 }
 
+
 function clickSubmit() {
-    const book_title = document.querySelector('.book_title');
+    const book_title = document.getElementById('book_title');
     const book_author = document.getElementById('book_author');
     const book_pages = document.getElementById('book_pages');
     const book_read = document.getElementById('book_read');
 
     addBookToLibrary(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked));
-    createList();
+    display_cells(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked));
 }
 
 const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true);
@@ -57,7 +53,8 @@ addBookToLibrary(book1);
 const book2 = new Book('Ovarian Medusa', 'Ashem Boshce', 532, true);
 addBookToLibrary(book2);
 
-createList();
+display_cells(book1);
+display_cells(book2);
 
 const submit = document.querySelector('button');
 
