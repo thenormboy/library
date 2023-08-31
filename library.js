6078b76ca86bf7ab64f94d7a4a91d3afbeae1d3a
Bleaze Perris <bperriscode@gmail.com>
2023-08-31 16:05:00 -0400

    added removal of books functionality

--- library.js
+++ library.js
@@ -1,18 +1,20 @@
 const myLibrary = [];
 const container = document.querySelector('.container');
-libIndex = 0;
+let libIndex = 0;
 
-function Book(title, author, pages, read, libraryIndex) {
+function Book(title, author, pages, read, libraryIndex, isDisplayed, toBeRemoved) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.libraryIndex = libraryIndex
     this.isDisplayed = isDisplayed
+    this.toBeRemoved = toBeRemoved
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    libIndex = libIndex + 1;
 }
 
 function removeBookFromLibrary(book) {
@@ -20,32 +22,36 @@ function removeBookFromLibrary(book) {
 }
 
 function updateLibrary(library) {
-
+    
     library.forEach(updateBook);
 
     function updateBook(value) {
-        if (value.isDisplayed == false && value.libIndex > -1) {
+        
+        if (value.isDisplayed === false && value.libraryIndex > -1) {
             value.isDisplayed = true;
             removeCells();
             displayCells(library);
-        } else if (value.isDisplayed == true ) {
+        } else if (value.isDisplayed === true && value.toBeRemoved === true) {
             removeBookFromLibrary(value);
             removeCells();
             displayCells(library)
         }
-
     }
 }
 
 function displayCells(library) {
 
+    libIndex = 0;
+
     library.forEach(displayCell);
 
     function displayCell(value) {
 
+        value.libraryIndex = libIndex;
+
         const content = document.createElement('tr');
         content.classList.add('fullcell')
-        content.classList.add(libIndex)
+        content.setAttribute('id', value.libraryIndex)
     
         const display_title = document.createElement('td');
         const display_author = document.createElement('td');
@@ -66,12 +72,14 @@ function displayCells(library) {
         content.appendChild(display_read)
     
         remove_button.innerText = 'x'
-        remove_button.classList.add(libIndex)
+        remove_button.classList.add(value.libraryIndex)
         remove_button.classList.add('remove')
+
+        remove_button.onclick = function() {clickRemove(value.libraryIndex)}
+
         content.appendChild(remove_button)
-    
         container.appendChild(content);
-    
+
         libIndex = libIndex + 1;
     }
 }
@@ -79,9 +87,9 @@ function displayCells(library) {
 function removeCells() {
 
     const content = document.getElementsByClassName('fullcell')
-    Array.from(content).forEach(removeChild);
+    Array.from(content).forEach(removeCell);
 
-    function removeChild(value) {
+    function removeCell(value) {
         container.removeChild(value);
     }
 }
@@ -92,34 +100,39 @@ function clickSubmit() {
     const book_pages = document.getElementById('book_pages');
     const book_read = document.getElementById('book_read');
 
-    addBookToLibrary(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked));
+    addBookToLibrary(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked, libIndex, false, false));
+    updateLibrary(myLibrary);
 }
 
 function clickRemove(removeButtonIndex) {
-    const content= document.getElementsByClassName('fullcell')
 
-    Array.from(content).forEach(removeChild);
+    let element = document.getElementById(removeButtonIndex)
 
-    function removeChild(value) {
-        if (value.classList[1] == removeButtonIndex) {
+    myLibrary.forEach(switchRemove);
 
-            container.removeChild(value);
+    function switchRemove(value) {
+        if (value.libraryIndex == removeButtonIndex) {
+            value.toBeRemoved = true;
         }
     }
+
+    element.remove();
+
 }
 
-const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true, libIndex, false);
+const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true, libIndex, false, false);
 addBookToLibrary(book1);
 
-const book2 = new Book('Ovarian Medusa', 'Ashem Boshce', 532, true, libIndex, false);
+const book2 = new Book('Ovarian Medusa', 'Ashem Boshce', 532, true, libIndex, false, false);
 addBookToLibrary(book2);
 
-displayCells(myLibrary)
+const book3 = new Book('Sickness and Syncope', 'Ashem Boshce', 67, true, libIndex, false, false);
+addBookToLibrary(book3);
 
+updateLibrary(myLibrary)
 
 const submit = document.querySelector('.submit');
 
 submit.addEventListener('click', () => {
     clickSubmit();
 })
-
