1a1264341262a6130e4b886b174e7e37a3fe2b49
Bleaze Perris <bperriscode@gmail.com>
2023-08-31 11:52:05 -0400

    reworked code to delete table and create a new one when updating books

--- library.js
+++ library.js
@@ -8,45 +8,82 @@ function Book(title, author, pages, read, libraryIndex) {
     this.pages = pages
     this.read = read
     this.libraryIndex = libraryIndex
+    this.isDisplayed = isDisplayed
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayCells(book) {
+function removeBookFromLibrary(book) {
+    myLibrary.splice(myLibrary.indexOf(book), 1);
+}
 
-    const content = document.createElement('tr');
-    content.classList.add('fullcell')
-    content.classList.add(libIndex)
+function updateLibrary(library) {
 
-    const display_title = document.createElement('td');
-    const display_author = document.createElement('td');
-    const display_pages = document.createElement('td');
-    const display_read = document.createElement('td');
-    const remove_button = document.createElement('button');
+    library.forEach(updateBook);
 
-    display_title.innerText = book.title
-    content.appendChild(display_title)
+    function updateBook(value) {
+        if (value.isDisplayed == false && value.libIndex > -1) {
+            value.isDisplayed = true;
+            removeCells();
+            displayCells(library);
+        } else if (value.isDisplayed == true ) {
+            removeBookFromLibrary(value);
+            removeCells();
+            displayCells(library)
+        }
 
-    display_author.innerText = book.author
-    content.appendChild(display_author)
+    }
+}
 
-    display_pages.innerText = book.pages
-    content.appendChild(display_pages)
+function displayCells(library) {
 
-    display_read.innerText = book.read
-    content.appendChild(display_read)
+    library.forEach(displayCell);
 
-    remove_button.innerText = 'x'
-    remove_button.classList.add(libIndex)
-    remove_button.classList.add('remove')
-    content.appendChild(remove_button)
+    function displayCell(value) {
 
-    container.appendChild(content);
+        const content = document.createElement('tr');
+        content.classList.add('fullcell')
+        content.classList.add(libIndex)
+    
+        const display_title = document.createElement('td');
+        const display_author = document.createElement('td');
+        const display_pages = document.createElement('td');
+        const display_read = document.createElement('td');
+        const remove_button = document.createElement('button');
+    
+        display_title.innerText = value.title
+        content.appendChild(display_title)
+    
+        display_author.innerText = value.author
+        content.appendChild(display_author)
+    
+        display_pages.innerText = value.pages
+        content.appendChild(display_pages)
+    
+        display_read.innerText = value.read
+        content.appendChild(display_read)
+    
+        remove_button.innerText = 'x'
+        remove_button.classList.add(libIndex)
+        remove_button.classList.add('remove')
+        content.appendChild(remove_button)
+    
+        container.appendChild(content);
+    
+        libIndex = libIndex + 1;
+    }
+}
 
-    libIndex = libIndex + 1;
+function removeCells() {
 
+    const content = document.getElementsByClassName('fullcell')
+    Array.from(content).forEach(removeChild);
+
+    function removeChild(value) {
+        container.removeChild(value);
+    }
 }
 
 function clickSubmit() {
@@ -56,7 +93,6 @@ function clickSubmit() {
     const book_read = document.getElementById('book_read');
 
     addBookToLibrary(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked));
-    displayCells(new Book(book_title.value, book_author.value, book_pages.value, book_read.checked));
 }
 
 function clickRemove(removeButtonIndex) {
@@ -72,13 +108,13 @@ function clickRemove(removeButtonIndex) {
     }
 }
 
-const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true, libIndex);
+const book1 = new Book('The Adamantine Narsus', 'Johannes Korantin', 213, true, libIndex, false);
 addBookToLibrary(book1);
-displayCells(book1);
 
-const book2 = new Book('Ovarian Medusa', 'Ashem Boshce', 532, true, libIndex);
+const book2 = new Book('Ovarian Medusa', 'Ashem Boshce', 532, true, libIndex, false);
 addBookToLibrary(book2);
-displayCells(book2);
+
+displayCells(myLibrary)
 
 
 const submit = document.querySelector('.submit');
